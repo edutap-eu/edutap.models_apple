653d984d2c611244dd5fc4fcb3d937898dd0fb38
Philipp Auersperg-Castell <phil@bluedynamics.com>
2023-11-02 18:22:15 +0100

    attachments

--- src/edutap/models_apple/template.py
+++ src/edutap/models_apple/template.py
@@ -1,3 +1,4 @@
+import base64
 from datetime import datetime
 import io
 from typing import Any
@@ -32,6 +33,7 @@ class PassTemplateBase(BaseModel):
     email: str = ""
     timestamp: datetime = Field(default_factory=datetime.now)
     pass_type: str = ""    # references Apple Pass Type Identifier as defined in models.@passmodel registry
+    blob: bytes = b""  # TODO: define blob structure model for sqlalchemy
     
     def create_pass(self, **kwargs):
         """create a pass from this template"""
@@ -54,7 +56,7 @@ class PassTemplateBase(BaseModel):
             if filename in ignorefiles:
                 continue
             with zf.open(filename) as f:
-                self.attachments[filename] = f.read()
+                self.attachments[filename] = base64.b64encode(f.read()).decode("utf-8")
         
         passjson = zf.read("pass.json")
 
@@ -62,17 +64,32 @@ class PassTemplateBase(BaseModel):
         self.pass_type = passobject.passType
         self.pass_json = passobject.model_dump(exclude_none=True)
     
+    
     @classmethod
     def from_passfile(cls, passfile: bytes | io.IOBase, template_id: str, backoffice_id: str):
+        """
+        constructor method to create a template from a passfile.
+        sutomatically sets pass_type, pass_json and attachments
+        """
         template = cls(template_id=template_id, backoffice_id=backoffice_id, pass_type="generic")
         template.import_passfile(passfile)
         
         return template
         
         
+    def attachment_filenames(self):
+        """return a list of all attachment filenames"""
+        return self.attachments.keys()
+    
+    
+    def get_attachment(self, filename: str) -> bytes:
+        """return an attachment by filename"""
+        b64str = self.attachments[filename]
+        data = base64.b64decode(b64str)
+        return data
         
         
 class PassTemplate(PassTemplateBase):
     """bare pydantic class for validation and serialization"""
     pass_json: dict[str, Any] = Field(default_factory=dict)# TODO: define pass_json structure model for sqlalchemy
-    attachments: dict[str, bytes] = Field(default_factory=dict)# TODO: define attachment structure model for sqlalchemy
+    attachments: dict[str, str] = Field(default_factory=dict)# TODO: define attachment structure model for sqlalchemy
